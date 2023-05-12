883ad3c76465b5db58097395f06bae35091c3a4d
Max Duchel <max.duchel98@gmail.com>
2023-05-12 11:53:12 +0200

    Carga inicial

---
+++ src/documentacion1/Circulo1.java
@@ -0,0 +1,32 @@
+
+/*
+ * Ejercicio de Documentación
+ * Circulos - Versión sin información
+ */
+package documentacion1;
+
+public class Circulo1 {
+
+    private double centroX;
+    private double centroY;
+    private double radio;
+
+    public Circulo1(double x, double y, double r) {
+        centroX = x;
+        centroY = y;
+        radio = r;
+    }
+
+    public double getCentroX() {
+        return centroX;
+    }
+
+    public double getPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    public void mueve(double despX, double despY) {
+        centroX = centroX + despX;
+        centroY = centroY + despY;
+    }
+}
